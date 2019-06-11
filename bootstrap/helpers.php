1d970fbdeeb2a57a011cc2a1ad12fa85bd56352e
mars <511391805@qq.com>
2019-06-11 17:10:01 +0800

    分类话题列表

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,6 +3,12 @@
  * @author Summer
  * @time 2019-06-10 23:35
  */
-function route_class(){
-    return str_replace('.','-',Route::currentRouteName());
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
+
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show')&& if_route_param('category',$category_id)))
 }
