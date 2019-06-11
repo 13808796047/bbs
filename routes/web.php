2e6c9e6d356eee4c7bcbb4063cc77f3b192bd5c7
mars <511391805@qq.com>
2019-06-11 09:05:19 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 
-Auth::routes(['verify'=>true]);
+Auth::routes(['verify' => true]);
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
