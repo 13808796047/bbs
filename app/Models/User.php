590bc8a27946b13f2993c27dcc760c190060199a
mars <511391805@qq.com>
2019-06-20 16:50:29 +0800

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,15 +28,17 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->laravelNotify($instance);
     }
 
+    public function markAsRead()
+    {
+        $this->notifications_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+
     protected $hidden = [
         'password', 'remember_token',
     ];
